6bef37c4799f581a3618960411db400e1c48ee6d
Robert Bandstra <rob.bandstra@gmail.com>
2021-01-14 18:50:23 -0600

    program takes user string and separates using hyphens and additional letters

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string.");
+            string input = Console.ReadLine();
+
+            var charIndex = 0;
+            for (var c = 0; c < input.Length; c++)
+            {
+                Console.Write($"{input[c].ToString().ToUpper()}");
+
+                while(true)
+                {
+                    
+                    if (charIndex < c)
+                    {
+                        Console.Write($"{input[c].ToString()}");
+
+                        charIndex++;
+                        continue;
+                    }
+                    break;
+                }
+
+                if (c != input.Length - 1)
+                {
+                    Console.Write($"-");
+                    charIndex = 0;
+                }
+                
+
+                
+            }
+
+            
+
+
+
+        }
+    }
+}
